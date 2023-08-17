cf9d23c5d28c10d58d65dac63eda58b8b1990068
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-08-17 18:10:32 +0300

    csv import

---
+++ app/Imports/DepositsImport.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Deposits;
+use Maatwebsite\Excel\Concerns\ToModel;
+
+class DepositsImport implements ToModel
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        return new Deposits([
+            
+        ]);
+    }
+}
