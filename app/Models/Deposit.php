41634f765da7d014eb597246a7b7f17ba4696de4
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-08-17 20:45:46 +0300

    Create Deposit.php

---
+++ app/Models/Deposit.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Deposit extends Model
+{
+    protected $table = 'deposits';
+
+    protected $fillable = [
+        'amount',
+        'member_id', // Adjust this based on the correct column name in your database
+    ];
+
+    protected $casts = [
+        'amount' => 'double',
+    ];
+
+    // Define timestamps
+    public $timestamps = true;
+
+    // Define the relationship with Member model
+    public function member()
+    {
+        return $this->belongsTo(Member::class, 'member_id', 'id');
+    }
+}
