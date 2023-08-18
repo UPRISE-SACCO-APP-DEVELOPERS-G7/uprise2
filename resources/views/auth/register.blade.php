bd5507e9c965fe50532615f8a5fc40abafdc83a2
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-08-18 15:07:27 +0300

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,4 @@
-@extends('layouts.app', ['activePage' => 'register', 'title' => 'Light Bootstrap Dashboard Laravel by Creative Tim &
-UPDIVISION'])
+@extends('layouts.app', ['activePage' => 'register', 'title' => 'Register'])
 
 @section('content')
 <div class="full-page register-page section-image" data-color="orange"
