e997d6dddb57ccd43ba1848568e1188692e01c82
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-08-19 21:52:54 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -25,14 +25,14 @@ Auth::routes();
 
 Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('dashboard');
 
-Route::group(['middleware' => 'auth'], function () {
+Route::group(['middleware' => ['prevent-back-history', 'auth']], function () {
 	Route::resource('user', 'App\Http\Controllers\UserController', ['except' => ['show']]);
 	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'App\Http\Controllers\ProfileController@edit']);
 	Route::patch('profile', ['as' => 'profile.update', 'uses' => 'App\Http\Controllers\ProfileController@update']);
 	Route::patch('profile/password', ['as' => 'profile.password', 'uses' => 'App\Http\Controllers\ProfileController@password']);
 });
 
-Route::group(['middleware' => 'auth'], function () {
+Route::group(['middleware' => ['prevent-back-history', 'auth']], function () {
 	Route::get('{page}', ['as' => 'page.index', 'uses' => 'App\Http\Controllers\PageController@index']);
 	// Route::get('/loans', 'App\Http\Controllers\PageController@loans')->name('all_loans');
 	Route::get('/loans', 'App\Http\Controllers\LoansController@loans')->name('all_loans');
